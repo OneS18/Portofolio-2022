dc67274e1f6b9efe8413d148070af792a2ea5ebd
OneS18 <diablo201888@gmail.com>
2022-05-28 17:39:59 +0300

    add some css for work component

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import "../styles/Work.css";
 import WorkImg from "../assets/workImg.jpeg";
 import realEstate from "../assets/realestate.jpg";
 import Netflix from "../assets/netflix.png";
@@ -9,7 +10,10 @@ import Calculator from "../assets/calculator.png";
 
 const Work = () => {
   return (
-    <div name="work" className="w-full md:h-screen text-gray-300 bg-[#0a192f]">
+    <div
+      name="work"
+      className="projects w-full md:h-screen text-gray-300 bg-[#0a192f]"
+    >
       <div className="max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full">
         <div className="pb-8">
           <p className="text-4xl font-bold inline border-b-4 text-gray-300 border-pink-600">
